b727d0f8359094370c030ccd2d171c1e18b8accc
Aviad Hahami <aviadhahami@gmail.com>
2016-06-15 02:22:47 +0300

    Error cause splash screen wasn't in capital letter Thanks @Hirohito ! :)

--- components/app.js
+++ components/app.js
@@ -8,21 +8,18 @@ import {
 	Text,
 	View
 } from 'react-native';
+import SplashScreen from './splashScreen/splashScreen'
 
 class App extends Component{
 	render(){
 		return(
-			<View style={styles.container}>
-				<Text style={styles.welcome}>
-					Welcome to React Native!
-				</Text>
-				<Text style={styles.instructions}>
-					To get started, edit index.android.js
-				</Text>
-				<Text style={styles.instructions}>
-					Shake or press menu button for dev menu
-				</Text>
-			</View>
+			<SplashScreen>
+				<View style={styles.container}>
+					<Text style={styles.welcome}>
+						Welcome to React Native!
+					</Text>
+				</View>
+			</SplashScreen>
 		)
 	}
 }
@@ -38,11 +35,6 @@ const styles = StyleSheet.create({
 		fontSize: 20,
 		textAlign: 'center',
 		margin: 10,
-	},
-	instructions: {
-		textAlign: 'center',
-		color: '#333333',
-		marginBottom: 5,
 	},
 });
 
