8d1d7e51d63a73671c989c337412a3826de60530
Aviad Hahami <aviadhahami@gmail.com>
2016-06-15 02:22:55 +0300

    implementing splashScreen

--- components/splashScreen/splashScreen.js
+++ components/splashScreen/splashScreen.js
@@ -1,3 +1,53 @@
 /**
  * Created by aviad on 6/15/2016.
  */
+
+import React, { Component } from 'react';
+import {
+	StyleSheet,
+	Text,
+	View
+} from 'react-native';
+
+class SplashScreen extends Component{
+
+	constructor(props){
+		super(props);
+		this.state = {
+			done: false
+		}
+	}
+
+	timer() {
+		setTimeout(()=>{
+			this.setState({
+				done: true
+			});
+		},1000)
+	}
+	componentDidMount(){
+		this.timer();
+	}
+	render(){
+		return(
+			this.state.done ?
+				// If done -> Show all nested
+				({...this.props.chidren})
+				:
+				// Display Splash Screen
+				(<View>
+					<Text>
+						I'm Splash Screen
+					</Text>
+				</View>)
+
+		);
+	}
+
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default SplashScreen
