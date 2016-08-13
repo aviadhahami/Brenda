59cf771d08d653e98541d4223d64eb157898941f
Aviad Hahami <aviadhahami@gmail.com>
2016-08-13 15:33:53 +0300

    :heavy_plus_sign: installation attemps

--- android/app/src/main/java/com/brenda/MainApplication.java
+++ android/app/src/main/java/com/brenda/MainApplication.java
@@ -26,7 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new LinearGradientPackage()
+          new LinearGradientPackage(),
+          new VectorIconsPackage()
       );
     }
   };
