5c2f991de5f9cd4f7e1e340f72f861a6480408e2
Aviad Hahami <aviadhahami@gmail.com>
2016-08-13 13:37:27 +0300

    :construction: Adding navbar

--- components/Navigation/navigationContainer.js
+++ components/Navigation/navigationContainer.js
@@ -18,12 +18,33 @@ class NavigationContainer extends Component{
 		console.log(navigator);
 		return route.component
 	}
+	get _navigationBar(){
+		return 	<Navigator.NavigationBar
+			routeMapper={{
+				LeftButton: this._leftNavButton.bind(this),
+				RightButton: this._rightNavButtonConfig.bind(this),
+				Title: this._titleNavConfig.bind(this)
+			}}
+			style={{backgroundColor: 'purple'}}
+		/>
+	}
+	_titleNavConfig(route, navigator, index, navState){
+		return <Text style={{color:'white', textAlign:'center'}}>{route.title}</Text>
+	}
+	_leftNavButton(route, navigator, index, navState){
+		return <Text>Left</Text>
+	}
+	_rightNavButtonConfig(route, navigator, index, navState){
+		return <Text>Right</Text>
+	}
+	
 	render(){
 		return(
 			<Navigator
 				initialRoute={componentsConfig.getInitialComponent}
 				initialRouteStack={componentsConfig.getComponents}
 				renderScene={this._sceneLogic.bind(this)}
+				navigationBar={this._navigationBar}
 			/>
 		)
 	}
