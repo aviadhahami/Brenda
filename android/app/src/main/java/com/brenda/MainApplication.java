ac5a0e717afdbd457e0635110189030762d2efa5
Aviad Hahami <aviadhahami@gmail.com>
2016-08-13 19:04:32 +0300

    :jack_o_lantern: Adding orientation locking

--- android/app/src/main/java/com/brenda/MainApplication.java
+++ android/app/src/main/java/com/brenda/MainApplication.java
@@ -27,7 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
           new LinearGradientPackage(),
-           new OrientationPackage(this)
+           new OrientationPackage()
       );
     }
   };
