578d584b3c6adedeefe618094a1313ca531cd856
Aviad Hahami <aviadhahami@gmail.com>
2016-08-13 19:27:52 +0300

    :electric_plug: Injection new componenet

---
+++ components/Navigation/controlPanel.js
@@ -0,0 +1,21 @@
+/**
+ * Created by aviad on 8/13/2016.
+ */
+
+import React, {Component} from 'react'
+import {Text} from 'react-native'
+
+class ControlPanel extends Component{
+	constructor(props){
+		super(props)
+	}
+	
+	render(){
+		return(
+			<Text>Test</Text>
+		)
+	}
+	
+}
+
+export default ControlPanel
--- components/PetSelection/PetSelection.js
+++ components/PetSelection/PetSelection.js
@@ -4,9 +4,12 @@
 
 
 import React, { Component } from 'react'
-import {ScrollView, Text, StyleSheet, Dimensions} from 'react-native'
+import {ScrollView, Text, StyleSheet, Dimensions, TouchableHighlight} from 'react-native'
 import Route from './../Navigation/Route'
 import Icon from 'react-native-vector-icons/FontAwesome'
+
+import Drawer from 'react-native-drawer'
+import ControlPanel from './../Navigation/controlPanel'
 
 let {height} = Dimensions.get('window');
 class PetSelection extends Component{
@@ -15,16 +18,30 @@ class PetSelection extends Component{
 	}
 	render(){
 		return(
-			<ScrollView style={{backgroundColor:'#424242', marginTop:60,height:height}}>
-				<Text>PetSelection</Text>
-			</ScrollView>
+			<Drawer
+				type="static"
+				content={<ControlPanel />}
+				openDrawerOffset={100}
+				styles={drawerStyles}
+				tweenHandler={Drawer.tweenPresets.parallax}>
+				
+				<ScrollView style={{backgroundColor:'#424242', marginTop:60,height:height}}>
+					<Text>PetSelection</Text>
+				</ScrollView>
+				
+			</Drawer>
 		)
 	}
 	
 }
 
 function leftButtonFunc(route, navigator, index, navState) {
-	return <Icon name="bars" size={30} color="white"></Icon>
+	let that = this;
+	return (
+	<TouchableHighlight onPress={that._drawer.open.bind(this)}>
+		<Icon name="bars" size={30} color="white"></Icon>
+	</TouchableHighlight>
+)
 }
 function rightButtonFunc(route, navigator, index, navState) {
 	return <Icon name="plus" size={30} color="white"></Icon>
@@ -34,5 +51,10 @@ const styles = StyleSheet.create({
 		backgroundColor:'green'
 	}
 });
+
+const drawerStyles = {
+	drawer: { shadowColor: '#000000', shadowOpacity: 0.8, shadowRadius: 3},
+};
+
 let petSelectionRoute = new Route(0,'Pet Selection','petSelection', <PetSelection />,leftButtonFunc, rightButtonFunc);
 export {petSelectionRoute};
