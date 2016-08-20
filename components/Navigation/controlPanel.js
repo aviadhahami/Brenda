2120a6ebce8afb89a00d4655589145bb753dedb5
Aviad Hahami <aviadhahami@gmail.com>
2016-08-20 15:25:23 +0300

    :smiling_imp: Drawer stubs working nicely

--- components/Navigation/controlPanel.js
+++ components/Navigation/controlPanel.js
@@ -3,7 +3,7 @@
  */
 
 import React, {Component} from 'react'
-import {Text} from 'react-native'
+import {Text, View, StyleSheet, ScrollView } from 'react-native'
 
 class ControlPanel extends Component{
 	constructor(props){
@@ -12,10 +12,42 @@ class ControlPanel extends Component{
 	
 	render(){
 		return(
-			<Text>Test</Text>
+			<ScrollView style={styles.controlPanel}>
+				<Text style={styles.controlPanelWelcome}>Test1</Text>
+				<Text style={styles.controlPanelWelcome}>Test1.2</Text>
+				<Text style={styles.controlPanelWelcome}>Test1.2</Text>
+				<Text style={styles.controlPanelWelcome}>Test1.2</Text>
+				<Text style={styles.controlPanelWelcome}>Test1.2</Text>
+				<Text style={styles.controlPanelWelcome}>Test1.2</Text>
+				<Text style={styles.controlPanelWelcome}>Test1.2</Text>
+				<Text style={styles.controlPanelWelcome}>Test2</Text>
+				<Text style={styles.controlPanelWelcome}>Test3</Text>
+				<Text style={styles.controlPanelWelcome}>Test4</Text>
+				<Text style={styles.controlPanelWelcome}>Test5</Text>
+			</ScrollView>
 		)
 	}
 	
 }
+
+const styles = StyleSheet.create({
+	controlPanel: {
+		marginTop:60,
+		width:300,
+		flex: 1,
+		// justifyContent:'flex-end',
+		backgroundColor:'rgba(255,255,255,0.85)',
+	},
+	controlPanelText: {
+		color:'rgba(0,0,0,0.9)',
+	},
+	controlPanelWelcome: {
+		fontSize: 20,
+		// textAlign: 'right',
+		margin: 25,
+		color:'black',
+		fontWeight:'bold',
+	},
+});
 
 export default ControlPanel
