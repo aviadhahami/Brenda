3677ca998dc4cbc4ddde9b393f9d99746d1a1f72
Aviad Hahami <aviadhahami@gmail.com>
2016-08-21 20:28:25 +0300

    :bug: Trying to bring heavy guns

--- stores/ImageStore.js
+++ stores/ImageStore.js
@@ -4,6 +4,8 @@
 import ImageAPI from './APIs/ImageAPI'
 import {createStore} from 'cartiv'
 
+let assign = require('object.assign/polyfill')();
+Object.assign = assign;
 
 const b64toBlob = (b64Data, contentType='', sliceSize=512) => {
 	const byteCharacters = atob(b64Data);
